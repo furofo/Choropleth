9fe73610dad4eb4e7e94fa02facbc935813c5277
furofo <chriscellphonerepair@gmail.com>
2020-04-01 18:35:22 -0400

    stored json in outside variable

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function() {
+    let json1;
     fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json')
       .then(response => response.json())
       .then(data => {
-          let json = JSON.parse(JSON.stringify(data));
-          console.log(json);
+          json1 = JSON.parse(JSON.stringify(data));
+          console.log(json1);
       })
 });
 
