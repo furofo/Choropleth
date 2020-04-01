b895ca15b981fd4316a3d4ba004d4805938533a1
furofo <chriscellphonerepair@gmail.com>
2020-04-01 18:49:24 -0400

    got map to appear

--- config.js
+++ config.js
@@ -1,11 +1,61 @@
 
 $(document).ready(function() {
     let json1;
+    let json2;
     fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json')
       .then(response => response.json())
       .then(data => {
           json1 = JSON.parse(JSON.stringify(data));
           console.log(json1);
-      })
+      });
+
+    fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json')
+      .then(response => response.json())
+      .then(data => {
+          json2 = JSON.parse(JSON.stringify(data));
+          console.log(json2);
+      });
+
+      var svg = d3.select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height");
+    
+    // Map and projection
+    var path = d3.geoPath();
+    var projection = d3.geoMercator()
+      .scale(70)
+      .center([0,20])
+      .translate([width / 2, height / 2]);
+    
+    // Data and color scale
+    var data = d3.map();
+    var colorScale = d3.scaleThreshold()
+      .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+      .range(d3.schemeBlues[7]);
+    
+    // Load external data and boot
+    d3.queue()
+      .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+      .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
+      .await(ready);
+    
+    function ready(error, topo) {
+    
+      // Draw the map
+      svg.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+          // draw each country
+          .attr("d", d3.geoPath()
+            .projection(projection)
+          )
+          // set the color of each country
+          .attr("fill", function (d) {
+            d.total = data.get(d.id) || 0;
+            return colorScale(d.total);
+          });
+        }
 });
 
