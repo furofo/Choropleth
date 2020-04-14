a8770d9b95d87cb8860ddb3b47c10f30e07879e3
furofo <chriscellphonerepair@gmail.com>
2020-04-13 20:19:02 -0400

    put tooltip and hover effect for it

--- config.js
+++ config.js
@@ -4,76 +4,23 @@ $(document).ready(function() {
   let svg = d3.select("svg");
   let width = 500;
   let height = 500;
-
-  
   var path = d3.geoPath();
-  console.log('this should be geopath?');
-  let feature = {
-    "type": "Feature",
-    "id": 18049,
-    "properties": {},
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
-        [
-          [
-            649.1057769641899,
-            236.3490012887262
-          ],
-          [
-            655.5730607623997,
-            235.65348589750747
-          ],
-          [
-            655.8029642049944,
-            237.6231387281017
-          ],
-          [
-            656.7525653809294,
-            237.49455605073354
-          ],
-          [
-            656.8525233994488,
-            238.4823047996072
-          ],
-          [
-            658.0720112253864,
-            238.33618812077972
-          ],
-          [
-            658.161973442054,
-            239.3122475353472
-          ],
-          [
-            654.4735225586854,
-            239.77397624044198
-          ],
-          [
-            654.6634427938725,
-            241.70271640096453
-          ],
-          [
-            649.6755376697508,
-            242.2112024432841
-          ],
-          [
-            649.1057769641899,
-            236.3490012887262
-          ]
-        ]
-      ]
-    }
-  }
-
+  let tooltip = d3.select(".svgdiv")
+                  .append("div")
+                  .style("position", "absolute")
+                  .style("z-index", "10")
+                  .style("visiblity", "hidden")
+                  .style("background", "#ddd")
+                  .text("a simple tooltip")
+                  .attr("id", "tooltip");
+                  
   const EDUCATION_FILE = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json'; // this is  alink to eductation file simpler the one i called json 1
   const COUNTY_FILE = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'; 
   d3.queue()
       .defer(d3.json, COUNTY_FILE)
       .defer(d3.json, EDUCATION_FILE)
       .await(ready);
-
   function ready(error, us, education){
-  
   if(error) throw error;
   svg.append("g")
       .selectAll("path")
@@ -82,7 +29,23 @@ $(document).ready(function() {
       .append("path")
       .attr("d", path)
       .attr("fill", "black")
-      .attr("class", "counties");
+      .attr("class", "counties")
+      .on("mouseover", function(d, i) {
+        d3.select(this).attr("fill", "red");
+        tooltip
+          .style("left", d3.event.pageX - 50 + "px")
+          .style("top", d3.event.pageY - 125 + "px")
+          .style("visiblity", "visible")
+          .style("display", "inline-block")
+          .style("background", "black")
+          .style("color", "white")
+          .style("opacity", "0.8")
+          .style("padding-left: 10px")
+          .style("padding-right: 10px")
+          .style("padding-bottom: 10px")
+          .style("padding-top: 10px")
+          .html("this is a simpet tool tip test")
+      });
   }
 });
 
