a153b677f7936bc1bfa6645c6c87e84677ace5f9
furofo <chriscellphonerepair@gmail.com>
2020-04-18 19:50:41 -0400

    reformatted tick labels to add % symbols

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
-
-  
   let body = d3.select("body");
   let svg = d3.select("svg");
-  let width = 500;
-  let height = 500;
   var path = d3.geoPath();
-  let tooltip = d3.select(".svgdiv")
+  let tooltip = d3.select(".svgdiv") //logic for tooltip later hidden by default
                   .append("div")
                   .style("position", "absolute")
                   .style("z-index", "10")
@@ -15,35 +11,36 @@ $(document).ready(function() {
                   .text("a simple tooltip")
                   .attr("id", "tooltip");
                   
-  const EDUCATION_FILE = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json'; // this is  alink to eductation file simpler the one i called json 1
-  const COUNTY_FILE = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'; 
-  d3.queue()
+  const EDUCATION_FILE = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json'; // this is  alink to eductation file 
+  const COUNTY_FILE = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json';  //this is link for counties file used to draw counties
+  d3.queue() // way to handles thing asyncronyls in v4 d3 expects an await function with typical paramter of ready and an optional number of args for each file/ json object it is waiting for
       .defer(d3.json, COUNTY_FILE)
       .defer(d3.json, EDUCATION_FILE)
       .await(ready);
-  function ready(error, us, education){
-  if(error) throw error;
-  let idMatch = function(currentData) {
-      let filteredEducation = education.filter((d) => d.fips == currentData.id);
-      return filteredEducation;
+  function ready(error, us, education){ // takes three args, first is error message if fails, 2nd is the County File called US, Third is EducationFILE called Education
+  if(error) throw error; // if an error tells us what it is
+  let idMatch = function(currentData) { // this is a function that takes a data object and compares its id with the fips of another object
+      let filteredEducation = education.filter((d) => d.fips == currentData.id); // in this case object it takes is county file and object it compares to is educatin
+      return filteredEducation; // it gets the object within education that mathces the id of counties basically 
   }
- // console.log("logging max and min of bacelors here");
-  //console.log(d3.max(education, d => d.bachelorsOrHigher)); //75.1 is max
-  //console.log(d3.min(education, d => d.bachelorsOrHigher)); // 2.6 is min
-  let colorScale = d3.scaleThreshold()
+  let colorScale = d3.scaleThreshold() // logic for color scale 2.6 is min value for Bachelors and 75.1 is max so wrote a threshold scale up to 60 to categorize this accordingly
                       .domain([10, 20, 30, 40, 50, 60])
                       .range(["#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", "#08306b"]);
-  //let colors = d3.scaleOrdinal(d3.schemeBlues[9]); // scale crhomatic color brewer color schme
-  // this is array of colors from schem blues  ["#f7fbff", "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", "#08306b"]
-  // wnant to use a threshold scale and threshold invert extent
-
-  
   svg.append("g")
       .selectAll("path")
-      .data(topojson.feature(us, us.objects.counties).features)
+      .data(topojson.feature(us, us.objects.counties).features) // topoJson is extension of GeJSon that uses line segments called arcs which objects from county_file contains multipe features
+                                // feature is method to convert TopoJSON to GEoJSON takes two arguments toplogy and object
+                                // toplogy is given us json object which has a type property of topology hence why it was used
+                                // it is given object argument of us.objects.coutneis which has property of type geomertyr collection and gemotries
+                                // in docs says  Then, if the object is a GeometryCollection, a FeatureCollection is returned, and each geometry in the collection is mapped to a Feature.
+                                // so this means because us.objects.counties is a gemotry collection it ruturns  a feature collection objcetct
+                                // this object has type and features properties
+                                // we want features for the data since these contains the svg d data we need to draw path later
       .enter()
       .append("path")
-      .attr("d", path)
+      .attr("d", path) // calls path on the current feature object which is d3.getpath which when geiven Geojson gemoetry or feature object generates the d path of SVG which has in
+                        // insturctions to draw the counties bascially, this loops through every single object in the features property of the topojson.feature object in the data argument
+                        // and draws its path
       .attr("fill", function(d) {
         let educationArr = idMatch(d);
         //console.log(" hey its me d again");
@@ -99,10 +96,12 @@ $(document).ready(function() {
   console.log('this is gscale');
   console.log(gXscale(10));
   let g = svg.append('g');
-  let xAxis = d3.axisBottom(gXscale)
-             // .tickValues(colorScale.domain().filter(function(d,i){  
+  let xAxis = d3.axisBottom(gXscale) 
     // only show every 10 tick or every 10th year
-               .tickValues( gXscale.ticks( 5 ).concat( gXscale.domain() ) );
+               .tickValues( gXscale.ticks( 5 ).concat( gXscale.domain() ) )
+              .tickFormat(function(d) {
+                return d + '%';
+              });
                 
   g.attr('id', "legend");
   
@@ -111,29 +110,23 @@ $(document).ready(function() {
       var d = colorScale.invertExtent(color);
       if(d[0]== undefined) {d[0] = 0};
       if(d[1]== undefined) {d[1] = 60};
-      console.log("this is d");
-      console.log(d);
       return d;
       
     }))
   .enter()
   .append("rect")
-  .attr('height', 30)
+  .attr('height', 20)
   .attr('fill', function(d) { return colorScale(d[0])})
   .attr('x', function(d) {return gXscale(d[0])})
   
   .attr('width', function(d) {
-   // console.log(d[1]);
-    //console.log(d[0]);
-   // console.log("here comes the x scale choo choo");
-   // console.log(gXscale(d[1]));
      return gXscale(d[1]) - gXscale(d[0]);
      })
-  .attr('transform', 'translate(570, 32)');
+  .attr('transform', 'translate(570, 22)');
 
   g.append("g")
     .attr('id', "x-axis")
-    .attr('transform', 'translate(570, 64)')
+    .attr('transform', 'translate(570, 44)')
     .call(xAxis);
   }
 
